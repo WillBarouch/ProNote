755e9472905f1fa8745fe1e84b51aa6c44f8cb6e
WillBarouch <93062709+WillBarouch@users.noreply.github.com>
2022-06-24 22:15:48 +1000

    add darkmode

--- main.js
+++ main.js
@@ -2,20 +2,22 @@ const notes = document.getElementById("notes");
 const title = document.getElementById('title');
 const selector = document.getElementById("nbsel");
 const buttons = document.getElementsByClassName("notebook-select")
+const dltg = document.getElementById("dltoggle")
+const r = document.querySelector(':root');
 class notebookclass {
     constructor(name,content) {
         this.name = name;
         this.content = content;
     }
 }
+let mode = 1; //set mode to 1 (darkmode)
 window.onload=function onLoad() {
     /*if (localStorage.getItem(title.value) !== null) {
         notes.textContent = localStorage.getItem(title.value);
     }*/
-    for (let i = 0; i < window.localStorage.length; i++) {
-
-        let key = window.localStorage.key(i);
-        let value = window.localStorage.getItem(key);
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        let value = localStorage.getItem(key);
         i = new notebookclass(key,value);
         let button = document.createElement("div");
         button.className = "notebooks";
@@ -24,6 +26,7 @@ window.onload=function onLoad() {
         selector.appendChild(button);
     }
 }
+
 setInterval(
 function setTitle() {
     document.getElementById("base").textContent = title.value;
@@ -52,4 +55,22 @@ for (let i = 0 ; i < buttons.length; i++) {
 function buttonclick(e){
     title.value = e.target.textContent;
     notes.textContent = localStorage.getItem(e.target.textContent)
+}
+
+dltg.addEventListener("click",dltoggle)
+
+function dltoggle() {
+    if (mode === 1) { // if darkmode is enabled
+        r.style.setProperty('--text', '#2E3440')
+        r.style.setProperty('--background', '#ECEFF4')
+        r.style.setProperty('--foreground', '#D8DEE9')
+        r.style.setProperty('--midground', '#E5E9F0')
+        mode = 0
+    } else if (mode === 0){ //if lightmode is enabled
+        r.style.setProperty('--text', '#ECEFF4')
+        r.style.setProperty('--background', '#2E3440')
+        r.style.setProperty('--foreground', '#4C566A')
+        r.style.setProperty('--midground', '#434C5E')
+        mode = 1
+    }
 }

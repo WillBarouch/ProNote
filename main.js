e3b1ded6a34d7f0d2df2111d028d4e375bd0a24d
WillBarouch <93062709+WillBarouch@users.noreply.github.com>
2022-06-24 22:17:45 +1000

    add semicolons

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 const notes = document.getElementById("notes");
 const title = document.getElementById('title');
 const selector = document.getElementById("nbsel");
-const buttons = document.getElementsByClassName("notebook-select")
-const dltg = document.getElementById("dltoggle")
+const buttons = document.getElementsByClassName("notebook-select");
+const dltg = document.getElementById("dltoggle");
 const r = document.querySelector(':root');
+
 class notebookclass {
     constructor(name,content) {
         this.name = name;
         this.content = content;
     }
 }
+
 let mode = 1; //set mode to 1 (darkmode)
 window.onload=function onLoad() {
     /*if (localStorage.getItem(title.value) !== null) {
@@ -30,47 +32,47 @@ window.onload=function onLoad() {
 setInterval(
 function setTitle() {
     document.getElementById("base").textContent = title.value;
-    return true
-},5)
+    return true;
+},5);
 
 
 
-let notebook = new notebookclass(document.getElementById("base").textContent,notes.value)
-notes.addEventListener("input",updateNotebook)
+let notebook = new notebookclass(document.getElementById("base").textContent,notes.value);
+notes.addEventListener("input",updateNotebook);
 
 function updateNotebook(e) {
     notebook.name = title.value;
     notebook.content = e.target.value;
-    console.log("Note " +notebook.name +" value updated to " + notebook.content)
+    console.log("Note " +notebook.name +" value updated to " + notebook.content);
 }
 
 setInterval(
     function save() {
-        localStorage.setItem(notebook.name,notebook.content)
-        console.log("saved " + notebook.name)
+        localStorage.setItem(notebook.name,notebook.content);
+        console.log("saved " + notebook.name);
     }, 5000)
 for (let i = 0 ; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", buttonclick)
+    buttons[i].addEventListener("click", buttonclick);
 }
 function buttonclick(e){
     title.value = e.target.textContent;
     notes.textContent = localStorage.getItem(e.target.textContent)
 }
 
-dltg.addEventListener("click",dltoggle)
+dltg.addEventListener("click",dltoggle);
 
 function dltoggle() {
     if (mode === 1) { // if darkmode is enabled
-        r.style.setProperty('--text', '#2E3440')
-        r.style.setProperty('--background', '#ECEFF4')
-        r.style.setProperty('--foreground', '#D8DEE9')
-        r.style.setProperty('--midground', '#E5E9F0')
-        mode = 0
+        r.style.setProperty('--text', '#2E3440');
+        r.style.setProperty('--background', '#ECEFF4');
+        r.style.setProperty('--foreground', '#D8DEE9');
+        r.style.setProperty('--midground', '#E5E9F0');
+        mode = 0;
     } else if (mode === 0){ //if lightmode is enabled
-        r.style.setProperty('--text', '#ECEFF4')
-        r.style.setProperty('--background', '#2E3440')
-        r.style.setProperty('--foreground', '#4C566A')
-        r.style.setProperty('--midground', '#434C5E')
-        mode = 1
+        r.style.setProperty('--text', '#ECEFF4');
+        r.style.setProperty('--background', '#2E3440');
+        r.style.setProperty('--foreground', '#4C566A');
+        r.style.setProperty('--midground', '#434C5E');
+        mode = 1;
     }
 }
